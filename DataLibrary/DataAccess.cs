049476b36b4c2417b6e3bc6d98c1b20f329db169
Joshhortt <joshhortt@yahoo.com>
2021-04-19 14:06:20 +0100

    Add DataAccess methods async

--- DataLibrary/DataAccess.cs
+++ DataLibrary/DataAccess.cs
@@ -13,21 +13,21 @@ namespace DataLibrary
 	{
 
 		// Method LoadData()
-		public List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+		public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
 		{
 			using (IDbConnection connection = new MySqlConnection(connectionString))
 			{
-				List<T> rows = connection.Query<T>(sql, parameters).ToList();
-				return rows;
+				var rows = await connection.QueryAsync<T>(sql, parameters);
+				return rows.ToList();
 			}
 		}
 
 		// Method SaveData()
-		public void SaveData<T>(string sql, T parameters, string connectionString)
+		public Task SaveData<T>(string sql, T parameters, string connectionString)
 		{
 			using (IDbConnection connection = new MySqlConnection(connectionString))
 			{
-				connection.Execute(sql, parameters);
+				return connection.ExecuteAsync(sql, parameters);
 			}
 		}
 	}
