e6866f1d6fa3113ae0ee0836f8819418abd309a8
Joshhortt <joshhortt@yahoo.com>
2021-04-19 13:40:38 +0100

    Add Library Dependency, new blazor page 'People.cs' and Dependency injection in startup.cs

--- DataLibrary/DataAccess.cs
+++ DataLibrary/DataAccess.cs
@@ -9,26 +9,26 @@ using System.Threading.Tasks;
 
 namespace DataLibrary
 {
-	public class DataAccess
+	public class DataAccess : IDataAccess
 	{
 
-        // Method LoadData()
-		public static List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+		// Method LoadData()
+		public List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
 		{
 			using (IDbConnection connection = new MySqlConnection(connectionString))
-            {
-                List<T> rows = connection.Query<T>(sql, parameters).ToList();
-                return rows;
-            }
-        }
+			{
+				List<T> rows = connection.Query<T>(sql, parameters).ToList();
+				return rows;
+			}
+		}
 
-        // Method SaveData()
-        public void SaveData<T>(string sql, T parameters, string connectionString)
-        {
-            using (IDbConnection connection = new MySqlConnection(connectionString))
-            {
-                connection.Execute(sql, parameters);
-            }
-        }
+		// Method SaveData()
+		public void SaveData<T>(string sql, T parameters, string connectionString)
+		{
+			using (IDbConnection connection = new MySqlConnection(connectionString))
+			{
+				connection.Execute(sql, parameters);
+			}
+		}
 	}
 }
